7ab41f672fdcceb82e434ecfd81020ec08eab93d
Palak <modipalak62@gmail.com>
2023-06-03 19:02:29 +0530

    new commit

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
     return (
         <div className="cta">
-            <a href="https://modipalak62.hackerresume.io/38c5d53f-4642-481b-9673-6bf6999b0a3a" download className="btn">
+            <a href="https://modipalak62.hackerresume.io/38c5d53f-4642-481b-9673-6bf6999b0a3a" download className="btn" target="_blank">
                 Download Resume
             </a>
             <a href="#contact" className="btn">
