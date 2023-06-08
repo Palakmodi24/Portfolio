3e252dcb5363bb693893dd510f7df13faaded9c9
Palak <modipalak62@gmail.com>
2023-06-08 19:48:43 +0530

    New update

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,9 +1,9 @@
 import React from "react";
-//import Resume from "../../assets/Resume_Palak_Modi_Updated.pdf";
+import Resume from "../assets/My Resume.pdf";
 const CTA = () => {
     return (
         <div className="cta">
-            <a href="https://modipalak62.hackerresume.io/38c5d53f-4642-481b-9673-6bf6999b0a3a" download className="btn" target="_blank">
+            <a href={Resume} download className="btn" target="_blank">
                 Download Resume
             </a>
             <a href="#contact" className="btn">
